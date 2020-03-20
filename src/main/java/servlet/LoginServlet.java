aa7bbd0441750ef29f7912e81814bc494967c140
lin <2053919517@qq.com>
2020-03-20 20:45:04 +0800

    test

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -1,5 +1,8 @@
 package servlet;
 
+import service.AdminService;
+import service.impl.AdminServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,13 +10,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "/login")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name=request.getParameter("name");
         String pwd=request.getParameter("pwd");
-        if ("admin".equals(name)&&"admin".equals(pwd)){
-            request.getRequestDispatcher("index.jsp").forward(request,response);
+        AdminService adminService=new AdminServiceImpl();
+        int count=adminService.login(name,pwd);
+        if (count>0){
+            request.getRequestDispatcher("success.jsp").forward(request,response);
         }
         else{
             response.getWriter().write("登陆失败!");
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
-<h2>登陆成功!</h2>
+<h2>Welcome</h2>
 </body>
 </html>
